9b46605d931c7ec21b51441f49793776033f4ad0
rabee561 <97986403+rabee561@users.noreply.github.com>
2023-04-11 22:12:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slick-example").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+    arrows: true, //
+  });
+});
